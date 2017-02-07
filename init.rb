0c119e115b84ff439c9213be109057f54d7e9754
Peter Sowah <petersowah@gmail.com>
2017-02-07 22:43:49 +0000

    add file creation feature

--- init.rb
+++ init.rb
@@ -3,11 +3,12 @@
 # LAUNCH THIS FILE FROM THE TERMINAL TO GET STARTED
 
 APP_ROOT = File.dirname(__FILE__)
-# require "#{APP_ROOT}/lib/guide.rb"
 
+# require "#{APP_ROOT}/lib/guide"
 # require File.join(APP_ROOT, 'lib', 'guide.rb')
+# require File.join(APP_ROOT, 'lib', 'guide')
 
-$:.unshift(File.join(APP_ROOT, 'lib'))
+$:.unshift( File.join(APP_ROOT, 'lib') )
 require 'guide'
 
 guide = Guide.new('restaurants.txt')
--- lib/guide.rb
+++ lib/guide.rb
@@ -1,21 +1,37 @@
+require 'restaurant'
 class Guide
 
-  def initialize(path = nil)
-
+  def initialize(path=nil)
+    # locate the restaurant text file at path
+    Restaurant.filepath = path
+    if Restaurant.file_usable?
+      puts "Found restaurant file."
+      # or create a new file
+    elsif Restaurant.create_file
+      puts "Created restaurant file."
+      # exit if create fails
+    else
+      puts "Exiting.\n\n"
+      exit!
+    end
   end
 
   def launch!
     introduction
-
+    # action loop
+    #   what do you want to do? (list, find, add, quit)
+    #   do that action
+    # repeat until user quits
     conclusion
   end
 
   def introduction
-    puts "\n\n <<< Welcome to the Food Radar >>>\n\n"
-    puts "This is an interactive guide to help you find the food you crave. \n\n"
+    puts "\n\n<<< Welcome to the Food Radar! >>>\n\n"
+    puts "This is an interactive guide to help you find the food you crave.\n\n"
   end
 
   def conclusion
-    puts "\n <<< Goodbye and enjoy your meal! >>> \n\n\n"
+    puts "\n<<< Goodbye and enjoy your meal! >>>\n\n\n"
   end
+
 end
