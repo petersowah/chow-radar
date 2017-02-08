f1d48d865e42e13da160d6330baa3f1e0ac52f8c
Peter Sowah <petersowah@gmail.com>
2017-02-08 03:17:33 +0000

    final steps

--- lib/guide.rb
+++ lib/guide.rb
@@ -31,32 +31,34 @@ class Guide
     # action loop
     result = nil
     until result ==:quit
-      action = get_action
-      result = do_action(action )
+      action, args = get_action
+      result = do_action(action, args)
     end
     conclusion
   end
 
   def get_action
-    action = nil
+    action, args = nil
 
     # request input until valid action is submitted
     until Guide::Config.actions.include?(action)
       puts 'Actions:' + Guide::Config.actions.join(', ') if action
       print '> '
       user_response = gets.chomp
-      action = user_response.downcase.strip
+      args = user_response.downcase.strip.split(' ')
+      action = args.shift
     end
 
-    return action
+    return action, args
   end
 
-  def do_action(action)
+  def do_action(action, args=[])
     case action
       when 'list'
         list
       when 'find'
-        puts 'Finding...'
+        keyword = args.shift
+        find(keyword)
       when 'add'
         add
       when 'quit'
@@ -67,15 +69,46 @@ class Guide
     end
   end
 
-  def list
+  # TODO: REFACTOR LIST METHOD
+  def list(args=[])
+    sort_order = args.shift
+    sort_order ||= 'name'
+    sort_order = 'name' unless %w['name', 'cuisine', 'price', 'location'].include?(sort_order)
+
     puts "\n Listing joints... \n\n".upcase
 
     restaurants = Restaurant.saved_restaurants
+
+    restaurants.sort! do |restaurant1, restaurant2|
+      case sort_order
+        when 'name'
+          restaurant1.name.downcase <=> restaurant2.name.downcase
+        when 'cuisine'
+          restaurant1.cuisine.downcase <=> restaurant2.cuisine.downcase
+        when 'price'
+          restaurant1.price.to_i <=> restaurant2.price.to_
+      end
+      end
     output_restaurant_table(restaurants)
+    puts "Sort using: 'list cuisine\n\n'"
   end
 
-  def find
-
+  # TODO : REFACTOR FIND METHOD
+  def find(keyword='')
+    output_action_header("Find a joint")
+    if keyword
+      restaurants = Restaurant.saved_restaurants
+      found = restaurants.select do |restaurant|
+        restaurant.name.downcase.include?(keyword.downcase) ||
+            restaurant.cuisine.downcase.include?(keyword.downcase) ||
+            restaurant.price.to_i <= keyword.to_i ||
+            restaurant.location.downcase.include?(keyword.downcase)
+      end
+      output_restaurant_table(found)
+    else
+      puts 'Use a key phrase to find a joint from the list.'
+      puts "Example: 'find fufu', 'find pizza', 'find kokonte', 'find tuo zaafi', 'find Ghana jollof', etc\n\n"
+    end
   end
 
   def add
@@ -110,6 +143,7 @@ class Guide
     print " " + "Price".ljust(6)
     print " " + "Location".rjust(20)+ "\n"
     puts "-" * 81
+
     restaurants.each do |restaurant|
       line =  " " << restaurant.name.titleize.ljust(30)
       line << " " + restaurant.cuisine.titleize.ljust(20)
